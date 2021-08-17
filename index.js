2c79ecbe4d685cb8bbe5661679757e1711586078
Sebastian Mariani <sebastian.mariani.94@gmail.com>
2021-08-17 17:50:03 +0100

    refine web scraper

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 const url =
-  "https://www.amazon.co.uk/Raspberry-Pi-ARM-Cortex-A72-Bluetooth-Micro-HDMI/dp/B07TC2BK1X/ref=sr_1_4?dchild=1&keywords=raspberry+pi&qid=1623167718&sr=8-4";
+  "https://www.amazon.co.uk/Raspberry-Pi-ARM-Cortex-A72-Bluetooth-Micro-HDMI/dp/B0899VXM8F/ref=sr_1_3?dchild=1&keywords=raspberry%2Bpi%2B4&qid=1629218773&sr=8-3&th=1";
 
 async function configBrowser() {
   const browser = await puppeteer.launch();
@@ -20,10 +20,10 @@ async function checkPrice(page) {
 
   let currentPrice = Number(poundPrice.replace(/[^0-9.-]+/g, ""));
 
-  if (currentPrice < 60) {
-    console.log("Buy it!", currentPrice);
+  if (currentPrice < 70) {
+    console.log("Buy it!", poundPrice);
   } else {
-    console.log("Don't buy it", currentPrice);
+    console.log("Don't buy it", poundPrice);
   }
 }
 
