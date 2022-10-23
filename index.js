ae9433023a84d8718762d5c18ecd409cddd7fe52
seb <seb@sebs-MacBook-Pro.local>
2022-10-23 13:07:01 +0100

    test

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
+// const cheerio = require("cheerio");
 
-const url =
-  "https://www.amazon.co.uk/gp/product/B00NPYDJ6U?pf_rd_r=3JJDZ5X1KKSTTHQMNEJG&pf_rd_p=6f2bfee9-92a9-4ec5-94b5-ed7bbe78d734&pd_rd_r=0fca5c77-4d69-4613-98ad-6a9f423bc62c&pd_rd_w=ta4U6&pd_rd_wg=7rGlb&ref_=pd_gw_unk";
+// const url = "https://about.gitlab.com/jobs/all-jobs/"
+// async function configBrowser(site) {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(site);
+//   return page;
+// }
 
-async function configBrowser() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  return page;
-}
+// async function checkPrice(page) {
+//   await page.reload();
+//   let html = await page.evaluate(() => document.body.innerHTML);
 
-async function checkPrice(page) {
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML);
+//   const poundPrice = await page.evaluate(
+//     () => document.querySelector("#priceblock_ourprice").textContent
+//   );
 
-  const poundPrice = await page.evaluate(
-    () => document.querySelector("#priceblock_ourprice").textContent
-  );
+//   let currentPrice = Number(poundPrice.replace(/[^0-9.-]+/g, ""));
 
-  let currentPrice = Number(poundPrice.replace(/[^0-9.-]+/g, ""));
+//   if (currentPrice < 70) {
+//     console.log("Buy it!", poundPrice);
+//   } else {
+//     console.log("Don't buy it", poundPrice);
+//   }
+// }
 
-  if (currentPrice < 70) {
-    console.log("Buy it!", poundPrice);
-  } else {
-    console.log("Don't buy it", poundPrice);
+// async function monitor() {
+//   let page = await configBrowser(url);
+//   console.log(page)
+// }
+
+// monitor();
+
+const request = require("request")
+const cheerio = require("cheerio")
+
+request("https://www.comeet.com/jobs/brandbastion/A7.008", (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html)
+
+    const items = $.html(".positionsGroupTitle")
+    console.log(items)
   }
-}
-
-async function monitor() {
-  let page = await configBrowser();
-  await checkPrice(page);
-}
-
-monitor();
+})
